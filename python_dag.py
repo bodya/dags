f3ae344318243a62b4c002d3c60a2f293bfd9c22
Evgeniy <bodya@inbox.ru>
2021-10-20 21:19:05 +0300

    Create python_dag.py

---
+++ python_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators import PythonOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'bodya',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 10, 20),
+    'retries': 0
+}
+
+dag = DAG('my_first_dag_name', 
+    default_args=default_args,
+    schedule_interval='* * * * *')
+    
+def hello():
+    print('Hello, world!')   
+    
+def sum_init():
+    return print(2+2)
+
+t1 = PythonOperator(task_id='my_first_task_id', python_callable=hello,
+                    dag=dag)
+                    
+t2 = PythonOperator(task_id='my_first_task_id', python_callable=sum_init,
+                    dag=dag)  
+                    
+t1 >> t2                    
