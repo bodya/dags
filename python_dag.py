d22119d03254f91f1d4eac77ccc29a537f8e70c6
Evgeniy <bodya@inbox.ru>
2021-10-20 21:33:38 +0300

    Update python_dag.py

--- python_dag.py
+++ python_dag.py
@@ -19,10 +19,12 @@ def hello():
 def sum_init():
     return print(2+2)
 
-t1 = PythonOperator(task_id='my_first_task_id', python_callable=hello,
+t1 = PythonOperator(task_id='my_first_task_print_id', python_callable=hello,
                     dag=dag)
                     
-t2 = PythonOperator(task_id='my_first_task_id', python_callable=sum_init,
+t2 = PythonOperator(task_id='my_first_task_sum_id', python_callable=sum_init,
                     dag=dag)  
-                    
-t1 >> t2                    
+       
+t3 = BashOperator(task_id='my_first_task_bash_id', bash_command='python3 print('Another hello world from same code!')',
+                    dag=dag)      
+t1 >> t2 >> t3                    
