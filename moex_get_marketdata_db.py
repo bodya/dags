551eda90e71d01c9abdb74377cc88361c8df642b
Evgeniy <bodya@inbox.ru>
2021-11-07 01:41:06 +0300

    update "datetime" type to moex_db.securities

--- moex_get_marketdata_db.py
+++ moex_get_marketdata_db.py
@@ -17,4 +17,5 @@ marketdata_df.columns = marketdata_df.columns.str.lower()
 marketdata_df.insert(0, 'datestamp', str(datetime.today().strftime('%Y-%m-%d %H:%M:%S')), True)
 marketdata_df.query('numtrades != 0', inplace=True)
 #записываем данные из pandas в clickhouse
-ph.to_clickhouse(marketdata_df, 'marketdata_tb', index=False, connection=connection)
+#ph.to_clickhouse(marketdata_df, 'marketdata_tb', index=False, connection=connection)
+ph.to_clickhouse(marketdata_df, 'temp_marketdata_tb', index=False, connection=connection)
