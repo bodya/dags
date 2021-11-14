138a310cb5580fd61da390c54ac4c77463310346
Evgeniy <bodya@inbox.ru>
2021-11-15 01:02:40 +0300

    clickhouse driver

--- moex_get_securities_db.py
+++ moex_get_securities_db.py
@@ -2,13 +2,15 @@ import json
 import requests
 import pandas as pd
 from datetime import datetime
-import pandahouse as ph
+from clickhouse_driver import Client
 
 
-connection = dict(database='db_moex',
-                  host='http://152.70.160.172:8123/',
-                  user='default',
-                  password='bodya')
+client = Client(host='152.70.160.172',
+               port=9000,
+               database='db_moex',
+               user='default',
+               password='bodya',
+               settings={'use_numpy': True})
 
 path = 'http://iss.moex.com/iss/engines/futures/markets/options/securities.json'
 req = requests.get(path)
@@ -16,5 +18,8 @@ json_data = json.loads(req.text)
 securities_df = pd.DataFrame(json_data['securities']['data'],columns=json_data['securities']['columns'])
 securities_df.columns = securities_df.columns.str.lower()
 securities_df.insert(0, 'datestamp', str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), True)
+
 #записываем данные из pandas в clickhouse
-ph.to_clickhouse(securities_df, 'securities_tb', index=False, connection=connection)
+client.insert_dataframe('INSERT INTO securities_tb VALUES', securities_df)
+#дропаем дубликаты
+client.execute('OPTIMIZE TABLE securities_tb DEDUPLICATE')
