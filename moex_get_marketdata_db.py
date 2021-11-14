7b6ed9fb278771636222b5c75564dcef379e8e04
Evgeniy <bodya@inbox.ru>
2021-11-15 00:57:19 +0300

    clickhouse driver

--- moex_get_marketdata_db.py
+++ moex_get_marketdata_db.py
@@ -2,21 +2,30 @@ import json
 import requests
 import pandas as pd
 from datetime import datetime
-import pandahouse as ph
+from clickhouse_driver import Client
 
 
-connection = dict(database='db_moex',
-                  host='http://152.70.160.172:8123/',
-                  user='default',
-                  password='bodya')
+client = Client(host='152.70.160.172',
+               port=9000,
+               database='db_moex',
+               user='default',
+               password='bodya',
+               settings={'use_numpy': True})
 
 path = 'http://iss.moex.com/iss/engines/futures/markets/options/securities.json'
 req = requests.get(path)
 json_data = json.loads(req.text)
 marketdata_df = pd.DataFrame(json_data['marketdata']['data'],columns=json_data['marketdata']['columns'])
 marketdata_df.columns = marketdata_df.columns.str.lower()
+marketdata_df.drop(['bid', 'offer', 'biddeptht', 'numbids', 'offerdepth', 'offerdeptht',
+                    'numoffers', 'settletoprevsettleprc', 'biddepth', 'settleprice', 'settletoprevsettle'],
+                   axis=1, inplace=True)
 marketdata_df.insert(0, 'datestamp', str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), True)
 marketdata_df.query('numtrades != 0', inplace=True)
+marketdata_df.fillna(0, inplace=True)
+
 #записываем данные из pandas в clickhouse
-ph.to_clickhouse(marketdata_df, 'marketdata_tb', index=False, connection=connection)
-#ph.to_clickhouse(marketdata_df, 'temp_marketdata_tb', index=False, connection=connection)
+client.insert_dataframe('INSERT INTO marketdata_tb VALUES', marketdata_df)
+#дропаем дубликаты
+
+client.execute('OPTIMIZE TABLE marketdata_tb DEDUPLICATE')
