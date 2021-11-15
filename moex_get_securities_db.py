6eb860a169154a129f61f03a9c3ede8436bc133c
Evgeniy <bodya@inbox.ru>
2021-11-15 23:54:15 +0300

    clickhouse driver

--- moex_get_securities_db.py
+++ moex_get_securities_db.py
@@ -3,9 +3,9 @@ import requests
 import pandas as pd
 from datetime import datetime
 from clickhouse_driver import Client
-#import pytz
+import pytz
 
-#tz_msk = pytz.timezone('Europe/Moscow')
+tz_msk = pytz.timezone('Europe/Moscow')
 client = Client(host='152.70.160.172',
                port=9000,
                database='db_moex',
@@ -18,8 +18,7 @@ req = requests.get(path)
 json_data = json.loads(req.text)
 securities_df = pd.DataFrame(json_data['securities']['data'],columns=json_data['securities']['columns'])
 securities_df.columns = securities_df.columns.str.lower()
-#securities_df.insert(0, 'datestamp', str(datetime.now(tz_msk).strftime('%Y-%m-%d %H:%M:%S')), True)
-securities_df.insert(0, 'datestamp', str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), True)
+securities_df.insert(0, 'datestamp', str(datetime.now(tz_msk).strftime('%Y-%m-%d %H:%M:%S')), True)
 #записываем данные из pandas в clickhouse
 client.insert_dataframe('INSERT INTO securities_tb VALUES', securities_df)
 #дропаем дубликаты
